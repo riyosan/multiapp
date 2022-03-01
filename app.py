58d49a8a9f1488ac33eba22a181c495b7982007a
riyosan <64902143+riyosan@users.noreply.github.com>
2022-03-01 20:20:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-st.set_page_config(page_title='Skripsi', layout='wide')
 from PIL import Image
 from multiapp import MultiApp
 from apps import praproses, data, prediksi # import your app modules here
@@ -8,7 +7,7 @@ app = MultiApp()
 
 # Add all your application here
 app.add_app("Preprocess", praproses.app)
-app.add_app("Train & Test", data.app)
-app.add_app("Predict", prediksi.app)
+app.add_app("Train & Test", train_test.app)
+app.add_app("Predict", predict.app)
 # The main app
 app.run()
