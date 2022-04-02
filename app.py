0f3cf3b6879e2d42f77fe57626f75507400dc9ba
riyosan <64902143+riyosan@users.noreply.github.com>
2022-04-02 15:31:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,39 @@
 import streamlit as st
+st.set_page_config(page_title='Skripsi', layout='wide')
+from PIL import Image
 from multiapp import MultiApp
-from apps import home, data, model # import your app modules here
+from apps import praproses, data, prediksi # import your app modules here
 
 app = MultiApp()
 
-st.markdown("""
-# Multi-Page App
 
-This multi-page app is using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework developed by [Praneel Nihar](https://medium.com/@u.praneel.nihar). Also check out his [Medium article](https://medium.com/@u.praneel.nihar/building-multi-page-web-app-using-streamlit-7a40d55fa5b4).
+image=Image.open('logo.png')
+logo = st.columns((1.5, 0.5))
+
+#page layout
+with logo[1]:
+	st.image(image,width=200)
+
+with logo[0]:
+	st.markdown("""
+# Skripsi
+
+Penggunaan Algoritma Stacking Ensemble Learning 
+""")
+	st.markdown("""
+Dalam Memprediksi Pengguna Enroll.
+
+""")
+	st.markdown("""
+""")
+	st.markdown("""
+**Riyo Santo Yosep - 171402020**
 
 """)
 
 # Add all your application here
-app.add_app("Home", home.app)
-app.add_app("Data", data.app)
-app.add_app("Model", model.app)
+app.add_app("Praproses", praproses.app)
+app.add_app("Latih & Uji", data.app)
+app.add_app("Prediksi", prediksi.app)
 # The main app
 app.run()
